45916c2ad8f36c1843d67e5e075506c03d9319e5
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-09 17:59:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function iLoveTheBeatles(num) {
     arr.push('I love the Beatles!');
     i++;
   }
-  while (i < (14-num));
+  while (i < (15-num));
   return arr;
 }
   if (num > 15) {
@@ -36,7 +36,7 @@ function iLoveTheBeatles(num) {
       arr.push('I love the Beatles!');
       i++
     }
-    while (i < (num - 14));
+    while (i < (num - 15));
 }
 }
 
