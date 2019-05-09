4680c4f6dce2fb2ca9028c72c04ed58033d3c5c4
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-09 17:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2) {
+  let foo = [];
+  for (let i = 0; i < arr1.length; i++){
+    let string = arr1[i] + " plays " + arr2[i];
+    foo.push(string);
+  }
+  return foo;
+}
+
+
+function johnLennonFacts(facts) {
+  let arr1 = [];
+  let counter = 0;
+  while (counter < facts.length) {
+    arr1.push(`${facts[counter]}!!!`);
+    counter++;
+  }
+  return arr1;
+}
+
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  let i;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  }
+  while (i < 15) {
+    if (num < 15);
+  }
+  return arr;
+  
+  if (num > 15) {
+    while (i < (15-num))
+    return arr;
+  
+  }
+  }
+
+
+
+
+
+
